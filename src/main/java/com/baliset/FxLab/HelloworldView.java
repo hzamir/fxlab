f31b93a3c7fc5e58d1dfa260dc9d9c2406af5ae9
Haim Zamir <hzamir@gmail.com>
2017-08-31 12:02:49 -0400

    my first Javafx commit

---
+++ src/main/java/com/baliset/FxLab/HelloworldView.java
@@ -0,0 +1,9 @@
+package com.baliset.fxlab;
+
+import de.felixroske.jfxsupport.AbstractFxmlView;
+import de.felixroske.jfxsupport.FXMLView;
+
+@FXMLView
+public class HelloworldView extends AbstractFxmlView {
+
+}
